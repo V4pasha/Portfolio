d9e2ea9f70bfc97693226a92e4749d7a4152287c
SHUVEB <114585995+V4pasha@users.noreply.github.com>
2023-04-28 12:50:45 +0530

    Add files via upload

---
+++ Port.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+	strings:["FRONTEND-DEVELOPER","JAVA-DEVELOPER","FULL-STACK-JAVA-DEVELOPER","EDITOR"],
+	typeSpeed:100,
+	backSpeed:100,
+	backDelay:1000,
+	loop:true
+});
---
+++ pra.java
@@ -0,0 +1,7 @@
+class pra{
+    public static void main(String[] args) {
+        String name="SHUVEB";
+        String str="";
+        System.out.println(name);
+    }
+}
